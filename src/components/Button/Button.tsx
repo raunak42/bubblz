a9eab0dd7c2473d58c6c40bca62b3e3b02a8468e
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-09-27 11:14:15 +0530

    Minor changes.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,6 @@
 export const Button: React.FC = () => {
   return (
-    <div className="bg-orange-600 rounded-xl text-white text-2xl font-bold px-6 py-4 flex items-center justify-center uppercase ">
+    <div className="hero-button bg-orange-600 rounded-xl text-white text-2xl font-bold px-6 py-4 flex items-center justify-center uppercase ">
       Order Pack
     </div>
   );
