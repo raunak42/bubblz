5cfa6b3a0c20b57709d77c7103352528074bcf13
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-09-27 11:14:00 +0530

    Text splitter function, to be used for animating text.

---
+++ src/components/TextSplitter/TextSplitter.tsx
@@ -0,0 +1,43 @@
+import clsx from "clsx";
+
+type Props = {
+  text: string;
+  className?: string;
+};
+
+export function TextSplitter({
+  text,
+  className,
+}: Props) {
+  if (!text) return null;
+
+  const words = text.split(" ");
+
+  return words.map((word: string, wordIndex: number) => {
+    const splitText = word.split("");
+    return (
+      <span
+        className={clsx("split-word", className)}
+        style={{  whiteSpace: "pre" }}
+        key={`${wordIndex}-${word}`}
+      >
+        {splitText.map((char, charIndex) => {
+          if (char === " ") return ` `;
+          return (
+            <span
+              key={charIndex}
+              className={`split-char inline-block split-char--${wordIndex}-${charIndex}`}
+            >
+              {char}
+            </span>
+          );
+        })}
+        {wordIndex < words.length - 1 ? (
+          <span className="split-char">{` `}</span>
+        ) : (
+          ""
+        )}
+      </span>
+    );
+  });
+}
