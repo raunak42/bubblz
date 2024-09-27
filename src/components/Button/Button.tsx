524e83ba964a5e0bf590a53c35eebf720dc74270
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-09-27 22:30:12 +0530

    Minor changes.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 export const Button: React.FC = () => {
   return (
-    <div className="hero-button bg-orange-600 rounded-xl text-white text-2xl font-bold px-6 py-4 flex items-center justify-center uppercase ">
+    <button
+      onClick={() => console.log("working")}
+      className="hero-button bg-orange-600 hover:bg-orange-700 rounded-xl text-white text-2xl font-bold px-6 py-4 flex items-center justify-center uppercase "
+    >
       Order Pack
-    </div>
+    </button>
   );
 };
