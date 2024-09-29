9976cb4194d5a880bbeda3e3bc4a67efc14fa253
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-09-29 15:14:53 +0530

    Bottom typography and footer.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,16 @@
-export const Footer:React.FC = ()=>{
-    return<div></div>
-}
+import CircleText from "../CircelText/CircleText";
+import Logo from "../Logo/Logo";
+
+export const Footer: React.FC = () => {
+  return (
+    <footer className="bg-[#FEE832] text-[#FE6334]">
+      <div className="relative mx-auto flex w-full max-w-4xl justify-center px-4 py-10">
+        <Logo className="w-[280px]" color="#FE6334" />
+
+        <div className="absolute  right-20 top-0 size-28 origin-center -translate-y-14 md:size-48 md:-translate-y-28">
+          <CircleText />
+        </div>
+      </div>
+    </footer>
+  );
+};
